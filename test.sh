ff9563732b444b74b1fddaefcb53ba57e12171e1
Blake Reimer <sobeksea@gmail.com>
2018-09-06 14:43:22 -0600

    Testing

--- test.sh
+++ test.sh
@@ -4,7 +4,9 @@
 # C=$(echo $CONTENT | sed 's/\//\\\//g')
 # sed "/<\/Package>/ s/.*/${C}\n&/" package.xml
 
-sed "/<types>/,/<\/types>/ s/<members>*<\/members>/<members>`git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`<\/members>/g;" package.xml
+files = `git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`
+
+sed "/<types>/,/<\/types>/ s/<members>*<\/members>/<members>$files<\/members>/g;" package.xml
 
 # while read -r line;
 # do
