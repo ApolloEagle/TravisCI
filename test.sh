7389f1c6212edb9d9dbf85de96bb85b7a6f7ac9f
Blake Reimer <sobeksea@gmail.com>
2018-09-06 14:20:40 -0600

    Adding changed files to XML

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
-# for file in `git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`
-# do
-# 	echo $file
-# done
+for file in `git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`
+do
+	CONTENT="<types>\n<members>$file</members>\n<name>Test Name</name>\n</types>"
+done
+
+C = $(echo $CONTENT | sed 's/\//\\\//g')
+sed "/<\/Students>/ s/.*/${C}\n&/" package.xml
 
 while read -r line;
 do
