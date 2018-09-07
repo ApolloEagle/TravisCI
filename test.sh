2a22fa75c9755e302385893bb3c1de684e0579ca
Blake Reimer <sobeksea@gmail.com>
2018-09-07 15:11:28 -0600

    Testing Travis

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-files = `git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`
+files=`git diff-tree --no-commit-id --name-only -r $TRAVIS_COMMIT`
 
 sed 's/<members>*<\/members>/<members>$files<\/members>/g' package.xml
