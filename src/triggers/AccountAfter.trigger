fdecec1876efa698e540b335bdfa381178ca812e
Blake Reimer <sobeksea@gmail.com>
2018-09-10 10:11:50 -0600

    Testing Travis

---
+++ src/triggers/AccountAfter.trigger
@@ -0,0 +1,2 @@
+trigger AccountAfter on Account (before insert, before update, after delete, after insert, after undelete, after update) {
+}
--- test.sh
+++ test.sh
@@ -9,6 +9,11 @@ do
 		class=true
 		classes+=( ${files#*src/classes/} )
 	fi
+
+	if [[ $files = *"src/triggers/"* ]]; then
+		trigger=true
+		triggers+=( ${files#*src/triggers/} )
+	fi
 done
 
 if [[ '$class'=true ]]; then
@@ -16,6 +21,14 @@ if [[ '$class'=true ]]; then
 	for (( i=0; i<${#classes[@]}; i++))
 	do
 		sed -i '/ApexClass/i \\t\t<members>'${classes[$i]}'</members>' src/package.xml
+	done
+fi
+
+if [[ '$trigger'=true ]]; then
+	sed -i '/<version>/i \\t<types>\n\t\t<name>ApexTriggers</name>\n\t</types>' src/package.xml
+	for (( i=0; i<${#triggers[@]}; i++))
+	do
+		sed -i '/ApexTriggers/i \\t\t<members>'${triggers[$i]}'</members>' src/package.xml
 	done
 fi
 
