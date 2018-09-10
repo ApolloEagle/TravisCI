2010ddbae87de7b1cbd0e2d64f2667d1da646c07
Blake Reimer <sobeksea@gmail.com>
2018-09-10 09:44:05 -0600

    Testing Travis

--- test.sh
+++ test.sh
@@ -1,5 +1,35 @@
-files=`git diff-tree --no-commit-id --name-only -r 2a22fa75c9755e302385893bb3c1de684e0579ca`
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<Package xmlns="http://soap.sforce.com/2006/04/metadata">
+    <version>43.0</version>
+</Package>' > src/package.xml
 
-if [[ $file = *'src/classes/'* ]]; then
-	sed "s/\(<members.*>\)[^<>]*\(<\/members.*\)/\1$files\2/" src/package.xml
+for files in `git diff-tree --no-commit-id --name-only -r 59778f656328c3430935590059ea1722023de29f`
+do
+	if [[ $files = *"src/classes/"* ]]; then
+		class=true
+		classes+=( ${files#*src/classes/} )
+	fi
+
+	if [[ $files = *"src/package"* ]]; then
+		page=true
+		pages+=( ${files#*src/} )
+	fi
+done
+
+if [[ '$class'=true ]]; then
+	sed -i '/<version>/i \\t<types>\n\t\t<name>ApexClass</name>\n\t</types>' src/package.xml
+	for (( i=0; i<${#classes[@]}; i++))
+	do
+		sed -i '/ApexClass/i \\t\t<members>'${classes[$i]}'</members>' src/package.xml
+	done
 fi
+
+if [[ '$page'=true ]]; then
+	sed -i '/<version>/i \\t<types>\n\t\t<name>ApexPage</name>\n\t</types>' src/package.xml
+	for (( i=0; i<${#pages[@]}; i++))
+	do
+		sed -i '/ApexPage/i \\t\t<members>'${pages[$i]}'</members>' src/package.xml
+	done
+fi
+
+cat src/package.xml
